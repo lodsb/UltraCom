056aea7ec8d2ab1f4a6fb45865dd8b36b86835f3
Niklas Klügel <lodsb@lodsb.org>
2010-12-10 17:49:29 +0100

    Starting dev on OSC Input II

---
+++ src/org/mt4j/input/inputSources/osc/OSCInputSource.java
@@ -0,0 +1,11 @@
+package org.mt4j.input.inputSources;
+
+import org.mt4j.MTApplication;
+
+public class OSCInputSource extends AbstractInputSource {
+
+	public OSCInputSource(MTApplication mtApp, String url) {
+		super(mtApp);
+	}
+
+}
