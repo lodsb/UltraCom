8b08cbbd1af53c600f65e4267594b53cce564cc8
btmo <bckr.tmo@gmail.com>
2013-11-05 19:55:18 +0100

    paths == 1 as special case

--- examples/ui/src/main/scala/Ui.scala
+++ examples/ui/src/main/scala/Ui.scala
@@ -120,19 +120,18 @@ object Ui extends Application with Persistability {
       var closestPath = paths.head /* set first path as initial closest path */
       var (closestConnection, argminParameter) = closestPath.closestSegment(point._1, point._2)
       var minDist = Vector.euclideanDistance(closestConnection(argminParameter), point)
-      println("paths: ")
-      paths.map{println(_)}
-      println("tail: " + paths.tail.toString)
-      /*paths.tail.foreach(path => { //for all the other paths in the list of paths
-        val (connection, parameter) = path.closestSegment(point._1, point._2) //get the point on the path that is closest to the specified coordinate as well as the segment of the path
-        val dist = Vector.euclideanDistance(connection(parameter), point)
-        if (minDist > dist){ //compare its distance to the current min distance
-          closestPath = path
-          closestConnection = connection
-          argminParameter = parameter
-          minDist = dist //and update if necessary
-        }
-      })	 */ 
+      if (paths.size > 1) {
+        paths.tail.foreach(path => { //for all the other paths in the list of paths
+          val (connection, parameter) = path.closestSegment(point._1, point._2) //get the point on the path that is closest to the specified coordinate as well as the segment of the path
+          val dist = Vector.euclideanDistance(connection(parameter), point)
+          if (minDist > dist){ //compare its distance to the current min distance
+            closestPath = path
+            closestConnection = connection
+            argminParameter = parameter
+            minDist = dist //and update if necessary
+          }
+        })
+      }
       Some((closestPath, closestConnection, argminParameter))
     }
     else None
