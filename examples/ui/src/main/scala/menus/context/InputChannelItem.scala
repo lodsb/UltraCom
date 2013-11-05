b90ab1a3660681fe763095cf338b2213795ea7a7
btmo <bckr.tmo@gmail.com>
2013-11-05 20:15:08 +0100

    fix

--- examples/ui/src/main/scala/menus/context/InputChannelItem.scala
+++ examples/ui/src/main/scala/menus/context/InputChannelItem.scala
@@ -100,7 +100,7 @@ class InputChannelItem(app: Application, menu: NodeContextMenu, center: Vector3D
     g.noStroke()
     g.ellipse(cx, cy, 2*this.radius, 2*this.radius)  
     
-    (1 to channelNumber).foreach(item => {
+    (1 to channelNumber + 1).foreach(item => {
       val (x,y) = this.positionOnCircle(cx, cy, 0.6f * this.radius, 2*math.Pi.toFloat, item, 4) //4 input channels
       g.ellipse(x, y, 4, 4)
     })
