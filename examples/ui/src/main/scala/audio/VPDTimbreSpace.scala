5186f29d439fdde1a6d14ce3e688c74cb91e4003
btmo <bckr.tmo@gmail.com>
2013-11-05 19:12:31 +0100

    color mix test failed, reverting back

--- examples/ui/src/main/scala/audio/VPDTimbreSpace.scala
+++ examples/ui/src/main/scala/audio/VPDTimbreSpace.scala
@@ -198,12 +198,12 @@ class VPDTimbreSpace extends TimbreSpace {
         val xDiff = x - xCoord
         val yDiff = y - yCoord
         if (xDiff*xDiff + yDiff*yDiff <= radius*radius) {
-          //val imgColor = this.argbToColor(image.get(x,y))
-          //val r = (imgColor.getR + color.getR)/2
-          //val g = (imgColor.getG + color.getG)/2
-          //val b = (imgColor.getB + color.getB)/2
-          //val a = (alpha + color.getAlpha)/2
-          val argb = this.colorToArgb(color)
+          val imgColor = this.argbToColor(image.get(x,y))
+          val r = (imgColor.getR + color.getR)/2
+          val g = (imgColor.getG + color.getG)/2
+          val b = (imgColor.getB + color.getB)/2
+          val a = (alpha + color.getAlpha)/2
+          val argb = this.colorToArgb(new MTColor(r,g,b,a))
           image.set(x,y,argb)
         }
       }
@@ -221,12 +221,12 @@ class VPDTimbreSpace extends TimbreSpace {
       for(y <- yCoord - radius to yCoord + radius) {
         val dist = math.abs(x - xCoord) + math.abs(y - yCoord)
         if (dist <= floatRadius) {
-          //al imgColor = this.argbToColor(image.get(x,y))
-          //val r = (imgColor.getR + color.getR)/2
-          //val g = (imgColor.getG + color.getG)/2
-          //val b = (imgColor.getB + color.getB)/2
-          //val a = (alpha + color.getAlpha)/2
-          val argb = this.colorToArgb(color)
+          val imgColor = this.argbToColor(image.get(x,y))
+          val r = (imgColor.getR + color.getR)/2
+          val g = (imgColor.getG + color.getG)/2
+          val b = (imgColor.getB + color.getB)/2
+          val a = (alpha + color.getAlpha)/2
+          val argb = this.colorToArgb(new MTColor(r,g,b,a))
           image.set(x,y,argb)
         }
       }
