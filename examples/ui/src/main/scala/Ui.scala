c80076522c2f69b56ce0052ed143304028c5931a
btmo <bckr.tmo@gmail.com>
2013-11-05 19:51:50 +0100

    println paths

--- examples/ui/src/main/scala/Ui.scala
+++ examples/ui/src/main/scala/Ui.scala
@@ -120,7 +120,8 @@ object Ui extends Application with Persistability {
       var closestPath = paths.head /* set first path as initial closest path */
       var (closestConnection, argminParameter) = closestPath.closestSegment(point._1, point._2)
       var minDist = Vector.euclideanDistance(closestConnection(argminParameter), point)
-      println("paths: " + paths.toString)
+      println("paths: ")
+      paths.map{println(_)}
       println("tail: " + paths.tail.toString)
       /*paths.tail.foreach(path => { //for all the other paths in the list of paths
         val (connection, parameter) = path.closestSegment(point._1, point._2) //get the point on the path that is closest to the specified coordinate as well as the segment of the path
