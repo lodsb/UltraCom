2ad77b1574a1b9d164ab8cc02d758e5610c59b9e
lodsb <lodsb@lodsb.org>
2013-12-14 00:48:15 +0100

    fixes and cleaning up

---
+++ src/main/scala/org/mt4j/components/visibleComponents/shapes/Rectangle.scala
@@ -0,0 +1,8 @@
+package org.mt4j.components.visibleComponents.shapes
+
+/**
+ * Created by lodsb on 12/13/13.
+ */
+class Rectangle {
+
+}
---
+++ src/main/scala/org/mt4j/util/animation/Transition.scala
@@ -0,0 +1,8 @@
+package org.mt4j.util.animation
+
+/**
+ * Created by lodsb on 12/11/13.
+ */
+class Transition {
+
+}
